1a6fa9e67832181010b7fc883fa6b21a441ae83d
r-a-m-a-2-5-6 <sivaramakrishnayelam8939@gmail.com>
2024-10-03 17:21:03 +0530

    nav menu-list

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,19 @@ import menuStart from '../assets/Navigation.svg'
 import menuStop from '../assets/menuStop.svg'
 
 import './navbar.css'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-    const [navigation, setNavigation] = useState<boolean>(false);
-
+    const [navigation, setNavigation] = useState<boolean>(true);
+    const onClick = () => {
+        const navMenu = document.querySelector(".nav-menu img") as HTMLElement;
+        if (navMenu) {
+          navMenu.addEventListener("click", () =>  {
+            setNavigation(!navigation)
+            
+          });
+        }
+      };
 
     return (
         <nav>
@@ -17,7 +26,17 @@ function Navbar() {
                     <img src={ceaLogo} alt='cea logo' />
                 </div>
                 <div className="nav-menu">
-                    <img src={navigation ? menuStop : menuStart} alt='cea logo' />
+                    <img src={navigation ? menuStop : menuStart} alt='cea logo' onClick={onClick} />
+                        <ul className={navigation ? "menu-list active" : "menu-list"}>
+                            <li><Link to="/"> HOME</Link></li>
+                            <li><Link to="#"> EVENTS</Link></li>
+                            <li><Link  to="/post"> POSTS</Link></li>
+                            <li><Link to="#"> STRUCTURAL ENGINEERING</Link></li>
+                            <li><Link to="#"> GEOTECHNICAL ENGINEERING</Link></li>
+                            <li><Link to="#"> WATER RESURCE ENGINEERING</Link></li>
+                            <li><Link  to="/team"> TEAMS</Link></li>
+                            <li><Link to="#"> CONTACT</Link></li>
+                    </ul>
                 </div>
             </div>
         </nav>
