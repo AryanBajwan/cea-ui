a37607be4de401558c857d0807c612c3b7891c6f
abhinesh612 <abhineshsharma612@gmail.com>
2024-10-08 19:05:06 +0530

    FIX: fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import heroImg from '../assets/hero.png'
-// import civilVid from '../assets/output.mp4'
-// import civilVid from '../assets/civil-1.mp4'
 
 import './home.css'
 
@@ -8,20 +6,32 @@ function Home() {
 
     return (
         <>
-            <div className="hero">
-                <div className="title">
-                    <h1>Civil</h1>
-                    <h1>Engineering</h1>
-                    <h1>Association</h1>
-                </div>
-                <div className='hero-img'>
-                    {/* <video autoPlay loop muted playsInline>
-                        <source src={civilVid} type="video/mp4"/>
-                    </video> */}
-                    <img className='tilt' src={heroImg} alt="Hero Image" />
-                </div>
-            </div>
+            <HeroSection />
+            <NewsSection />
         </>
+    );
+}
+
+const HeroSection = () => {
+    return (
+        <div className="hero">
+            <div className="title">
+                <h1>Civil</h1>
+                <h1>Engineering</h1>
+                <h1>Association</h1>
+            </div>
+            <div className='hero-img'>
+                <img className='tilt' src={heroImg} alt="Hero Image" />
+            </div>
+        </div>
+    );
+}
+
+const NewsSection = () => {
+    return (
+        <div>
+            <h1>News Section</h1>
+        </div>
     );
 }
 
